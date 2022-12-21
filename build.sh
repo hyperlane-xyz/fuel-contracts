476a9d29a91e7f045536ccfa71252b15d902d249
Yorke Rhodes <yorke@hyperlane.xyz>
2022-12-21 13:56:07 -0500

    Implement mailbox process (#17)
    * Implement mailbox process
    
    * Only log message id in process
    
    * Use encoded message and generic ISM abi
    
    * Use msg recipient specified ISM
    
    * Generic ABIs (#20)
    
    * Use workspaces for build
    
    * Mailbox process tests working (#35)
    
    Co-authored-by: Trevor Porter <trkporter@ucdavis.edu>

--- build.sh
+++
@@ -1,9 +0,0 @@
-packages=( merkle merkle-test bytes-extended hyperlane-message hyperlane-message-test hyperlane-mailbox )
-
-current_dir=$(pwd)
-
-for package in "${packages[@]}"
-do
-    echo "Building package: $package"
-    cd $current_dir/$package && forc fmt && forc build
-done
